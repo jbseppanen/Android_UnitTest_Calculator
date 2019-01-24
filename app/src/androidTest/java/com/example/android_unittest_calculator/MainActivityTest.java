f74e2bd3d3ea5e0ddcd8e054a4267636f9d871b0
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-24 13:30:39 -0800

    Add layout.  Layout copied and modified from online tutorials.

--- app/src/androidTest/java/com/example/android_unittest_calculator/MainActivityTest.java
+++ app/src/androidTest/java/com/example/android_unittest_calculator/MainActivityTest.java
@@ -2,6 +2,8 @@ package com.example.android_unittest_calculator;
 
 import org.junit.Rule;
 
+import androidx.test.rule.ActivityTestRule;
+
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
